0ca8a349f9b2ce2e68d95f224d4d54dadd05cb3d
El Yirdaw <127657148+justdoitel@users.noreply.github.com>
2023-07-10 03:55:32 -0700

    Rebuild rps using UI

--- code.js
+++ code.js
@@ -1,57 +1,106 @@
-function getComputerChoice (){
+function getRobotChoice (){
     let x = Math.floor(Math.random()*3+1);
     if(x==1) return "rock"
     if(x==2) return "paper"
     if(x==3) return "scissor"
 }
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection===computerSelection){
-        alert (`It was a tie. The computer also randomly chose ${computerSelection}.`)
-        return 0;
-    }
-    if (playerSelection === "rock"){ 
-        alert (`You chose ${playerSelection} while the computer chose ${computerSelection} so you ${computerSelection==="paper"?"lost":"won"}`)
-        return (computerSelection==="paper"?-1:1)
-    }
-    if (playerSelection === "paper"){ 
-        alert (`You chose ${playerSelection} while the computer chose ${computerSelection} so you ${computerSelection==="paper"?"lost":"won"}`)
-        return (computerSelection==="scissor"?-1:1)
-    }
-    if (playerSelection === "scissor"){ 
-        alert (`You chose ${playerSelection} while the computer chose ${computerSelection} so you ${computerSelection==="paper"?"lost":"won"}`)
-        return (computerSelection==="rock"?-1:1)
-    }
-}
+const humanPic = document.querySelector(".player img")
+const robotPic = document.querySelector(".robot img")
+const playerInstruction = document.querySelector(".player .instruction")
+const robotInstruction = document.querySelector(".robot .instruction")
+const playerScoreUI = document.querySelector(".player .score")
+const robotScoreUI = document.querySelector(".robot .score")
+const btns = document.querySelectorAll(".btns input")
+const resetbtn = document.querySelector("#reset")
 
-function game (){ //this game is best out of 5 (so a tie in one or three rounds can result in a tie of the whole game)
-    let playerTotal = 0, computerTotal = 0, tieTotal = 0;
-    for (let x = 0; x<5; x++){
-        let rock = 0, paper = 0, scissor = 0, pWon;
-        let playerchoice = prompt('Pick one from "Rock", "Paper", and "Scissor"',"Rock, Paper, Scissor").trim();
-        if(playerchoice.match(/rock/i)) rock++;
-        if(playerchoice.match(/paper/i)) paper++;
-        if(playerchoice.match(/scissor/i)) scissor++;
-        if(rock|paper|scissor){
-            if (rock+paper+scissor === 1){
-                if (rock) pWon=playRound ("rock",getComputerChoice())
-                else if (paper) pWon=playRound ("paper",getComputerChoice())
-                else pWon=playRound ("scissor",getComputerChoice())
-                if(pWon===1) playerTotal++;
-                if(pWon===-1) computerTotal++;
-                if(pWon===0) tieTotal++;
-            } else{
-                alert("invalid entry. please pick ONLY one selection from 'Rock', 'Paper', and 'Scissor'.");
-                x--;
-                continue;
-            }
-        } else {
-            alert("invalid entry. please pick from 'Rock', 'Paper', or 'Scissor'.");
-            x--;
-            continue;
+let playerScore = 0;
+let robotScore = 0;
+
+function playRound(playerSelection, robotSelection) { //TODO: make this into a function that returns number (so another function deals w UI and incrementing)
+    humanPic.src = `${playerSelection}.png`
+    robotPic.src = `${robotSelection}.png`
+    if(playerSelection===robotSelection){
+        playerInstruction.textContent = "Tie"
+        robotInstruction.textContent = "Tie"
+    }
+
+    if (playerSelection==="rock"){
+       if(robotSelection==="paper"){
+            playerInstruction.textContent = "Lost"
+            robotInstruction.textContent = "Won"
+            robotScore++;
+       }
+       else if(robotSelection==="scissor"){
+            robotInstruction.textContent = "Lost"
+            playerInstruction.textContent = "Won"
+            playerScore++;
+       }
+    }
+    else if (playerSelection === "paper"){ 
+        if(robotSelection==="rock"){
+            robotInstruction.textContent = "Lost"
+            playerInstruction.textContent = "Won"
+            playerScore++;
+        }
+        if(robotSelection==="scissor"){
+            playerInstruction.textContent = "Lost"
+            robotInstruction.textContent = "Won"
+            robotScore++;
         }
     }
-    alert(`Player ${playerTotal} - ${computerTotal} Computer \n${tieTotal} ${tieTotal===1?"tie":"ties"}.`)
+    else if (playerSelection === "scissor"){ 
+        if(robotSelection==="rock"){
+            playerInstruction.textContent = "Lost"
+            robotInstruction.textContent = "Won"
+            robotScore++;
+        }
+        if(robotSelection==="paper"){
+            robotInstruction.textContent = "Lost"
+            playerInstruction.textContent = "Won"
+            playerScore++;
+        }
+    }
+    playerScoreUI.textContent = playerScore;
+    robotScoreUI.textContent = robotScore;
+
+    if(robotScore===5||playerScore===5){
+        if(playerScore===5){
+            document.querySelector(".player .scoring").style.color ="red";
+            playerInstruction.style.color ="red";
+        } else {
+            document.querySelector(".robot .scoring").style.color ="red";
+            robotInstruction.style.color ="red";
+        }
+        resetbtn.style.display = "inline-block"
+        for (btn of btns){
+            btn.disabled = true;
+            btn.classList.remove("hoverable");
+        }
+    }
 }
 
-game();
+
+for (btn of btns){
+    btn.addEventListener("click",(event)=>(playRound(event.currentTarget.dataset.choice,getRobotChoice ())))
+}
+
+resetbtn.addEventListener("click",()=>{
+    document.querySelector(".player .scoring").style.color ="black";
+    playerInstruction.style.color ="black";
+    document.querySelector(".robot .scoring").style.color ="black";
+    robotInstruction.style.color ="black";
+    resetbtn.style.display = "none";
+    for (btn of btns){
+        btn.disabled = false;
+        btn.classList.add("hoverable");
+    }
+    playerScore=0;
+    robotScore=0;
+    robotInstruction.textContent = "Waiting..."
+    playerInstruction.textContent = "Choose a weapon!"
+    playerScoreUI.textContent = playerScore;
+    robotScoreUI.textContent = robotScore;
+    humanPic.src="Skull.png"
+    robotPic.src="Robot.png"
+})
